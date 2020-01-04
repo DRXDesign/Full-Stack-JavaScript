bfaaac364c2f5586637ac7a75cafa39b8c7bf098
Ziane Maamar <drx-design@hotmail.com>
2020-01-04 19:39:18 +0100

    The Mad Libs Challenge Revisited

--- 1- JavaScript Basics/3- Working with Numbers/scripts.js
+++ 1- JavaScript Basics/3- Working with Numbers/scripts.js
@@ -12,3 +12,18 @@ document.write("There are " + secondsPerDay + " Seconds in a day ");
 var myAge = 28;
 var alive = secondsPerMin * minesPerhour * hoursPerDays * daysPerWeek * weekPerYear * myAge;
 document.writeln("I've been alive " + alive + " secondes");
+
+
+//============================================
+/*  The Mad Libs Challenge Revisited  */
+//============================================
+var question = 3;
+var questionLeft = '[ ' + question + 'Question Left]';
+var adjective = prompt('Please type an adjective' + questionLeft);
+var verb = prompt('Please type a verbe');
+var noun = prompt('Please type a noun');
+alert('All done ready for the Message??');
+var sentence = "<h2> There was a " + adjective;
+sentence += ' programmer who wanted to use JavaScript to ' + verb;
+sentence += ' The ' + noun + ' .</h2>';
+document.write(sentence);
