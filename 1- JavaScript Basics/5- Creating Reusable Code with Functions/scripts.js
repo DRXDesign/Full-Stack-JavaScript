b0f0fded6f7f347b5e3f3db09246edb0ed05d021
Ziane Maamar <drx-design@hotmail.com>
2020-02-06 22:50:10 +0100

    finished chapter 1

--- 1- JavaScript Basics/5- Creating Reusable Code with Functions/scripts.js
+++ 1- JavaScript Basics/5- Creating Reusable Code with Functions/scripts.js
@@ -1,22 +1,50 @@
+
+/* Function */
 function alertRandom() {
     var randomNumber = Math.floor(Math.random() * 6) + 1;
-    alert(randumNumber)
+    alert(randomNumber)
 }
 alertRandom();
 
 function alertRandom() {
     var randomNumber = Math.floor(Math.random() * 6) + 1;
-    return randumNumber;
+    return randomNumber;
 }
 alertRandom();
 
+/*Function with parameter */
 function goToTheCoffeShop(drink, food) {
     alert(drink + " and " + food + " is on it's way");
 }
-gotToTheCoffeShop("Pepsi", "Pizza")
+goToTheCoffeShop("Pepsi", "Pizza")
+
+/* Clean function with paramenter that take a variable */
 
 function getRandomNumber(upper) {
     var randomNumber = Math.floor(Math.random() * upper) + 1;
-    return randumNumber;
+    return randomNumber;
 }
 alert(getRandomNumber(99999999));
+
+/* Scooped funtion */
+function greeting() {
+    var person = 'Maamar';
+    alert(person);
+}
+// Globle Scope
+var person = 'Amina';
+
+//always use var key word inside the function
+//===================================================
+/* Random Number Challenge                         */
+//===================================================
+
+function getRandomNumber(lower, upper) {
+    if (isNaN(upper) || isNaN(lower)) {
+        return message = ('Enter a number');
+    }
+
+    var randomNumber = Math.floor(Math.random() * (upper - lower + 1)) + lower;
+    return randomNumber;
+}
+alert(getRandomNumber("one", 100));
